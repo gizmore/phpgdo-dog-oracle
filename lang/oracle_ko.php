f50c4661bb45defc47483e460e3697ac4590a724
gizmore <gizmore@wechall.net>
2026-09-18 04:08:21 +0200

    'more korean'

---
+++ lang/oracle_ko.php
@@ -0,0 +1,26 @@
+<?php
+return [
+	'mt_dogoracle_newpoll' => '새 설문조사 만들기',
+	'msg_poll_created' => '설문조사가 생성되어 방송되었습니다.',
+	'dog_poll_created' => '새 설문(ID: %s): %s - %s%s %1$s, %s로 투표하세요.',
+	'mails_poll_created' => '새 설문조사',
+	'mailb_poll_created' => '친애하는 %s님,
+    
+    %s에 새 설문조사가 생성되었습니다.
+    
+    %s
+    
+    가능한 답변(%s을(를) 선택할 수 있음):
+    
+    %s
+    
+    다음 링크를 통해 투표하실 수 있습니다: %s
+    
+    친절한 감사
+    %2$s 시스템',
+	'err_dog_oracle_answer_count' => '%s개의 다른 선택에 답할 수 있습니다.',
+	'err_dog_oracle_answer_num' => '%s에서 %s 사이의 값만 선택할 수 있습니다.',
+	'msg_dog_voted' => '귀하의 투표가 기록되었습니다.',
+	'msg_dog_poll_created' => '설문조사가 생성되어 방송되었습니다.',
+	'msg_dog_poll_preview' => '귀하의 설문조사는 다음과 같습니다: %s - 답변: %s(NumChoices: %s). --save 스위치를 사용하여 생성합니다.',
+];
